087360fa1f24abeaab70d4e094b6b6bf98d0f36d
Slamet <slamet@pon.co.id>
2023-11-17 10:47:30 +0700

    first integrated commit

---
+++ components/SectionContainer.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from 'react';
+
+interface Props {
+  children: ReactNode;
+}
+
+export default function SectionContainer({ children }: Props) {
+  return (
+    <section className="mx-auto max-w-full px-0 sm:px-6 xl:max-w-full xl:px-0">
+      {children}
+    </section>
+  );
+}
---
+++ data/headerNavLinks.ts
@@ -0,0 +1,10 @@
+const headerNavLinks = [
+  { href: '/', title: 'Home' },
+  { href: '/ai-chat', title: 'AI Chat' },
+  { href: '/activities', title: 'Activities' },
+  { href: '/blog', title: 'Blog' },
+  { href: '/projects', title: 'Projects' },
+  { href: '/about', title: 'About' },
+];
+
+export default headerNavLinks;
