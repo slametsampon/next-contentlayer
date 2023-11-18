cca7629f1821d9e10f13f07f22f99d50c62b1d43
slametsampon <slamet@pon.co.id>
2023-11-18 09:48:26 +0700

    integrated Main - Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,24 @@
-import { allPosts } from '@/.contentlayer/generated'
-import Link from 'next/link'
+import { allBlogs, allPosts } from '@/.contentlayer/generated'
+import Link from '@/components/Link'
+import { allCoreContent, sortPosts } from 'pliny/utils/contentlayer'
+import Main from './Main'
+
 export default function Home() {
+  const sortedBlogs = sortPosts(allBlogs)
+  const blogs = allCoreContent(sortedBlogs)
   return (
-    <div className="prose dark:prose-invert">
-      {allPosts.map((post) => (
-        <article key={post._id}>
-          <Link href={post.slug}>
-            <h2>{post.title}</h2>
-          </Link>
-          {post.description && <p>{post.description}</p>}
-        </article>
-      ))}
-    </div>
+    <>
+      {/* <div className="prose dark:prose-invert">
+        {allPosts.map((post) => (
+          <article key={post._id}>
+            <Link href={post.slug} className="text-gray-900 dark:text-gray-100">
+              {post.title}
+            </Link>
+            {post.description && <p>{post.description}</p>}
+          </article>
+        ))}
+      </div> */}
+      <Main blogs={blogs} />
+    </>
   )
 }
